1f7b71046f78d7b4a15d6562dc0d4c710a1aaf3b
Tim <tim.blonski@gmail.com>
2016-04-03 14:40:00 -0400

    Convert currency string to Roman Numeral.

--- currency_repo.rb
+++ currency_repo.rb
@@ -5,15 +5,16 @@ class CurrencyRepo
     @currencies = set_currencies(lines)
   end
 
-  def to_roman(currencies = {})
-
+  def to_roman(currency_string)
+    # raise 'cannot convert' unless currency_string.is_a? String && currency_string =~ /glob|prok|pish|tegj/
+    currency_string.split(' ').map { |currency| @currencies[currency] }.join('')
   end
 
   def set_currencies(lines)
     lines.each_with_object({}) do |line, obj|
       currency = line.match(/glob|prok|pish|tegj/)[0]
       numeral = line.split(' ').last
-      obj[currency.to_sym] = numeral
+      obj[currency] = numeral
     end
   end
 end
--- test/currency_repo_test.rb
+++ test/currency_repo_test.rb
@@ -11,14 +11,18 @@ require './currency_repo'
       ]
       @repo = CurrencyRepo.new(lines)
       @currencies = {
-        glob: 'I',
-        prok: 'V',
-        pish: 'X',
-        tegj: 'L'
+        'glob' => 'I',
+        'prok' => 'V',
+        'pish' => 'X',
+        'tegj' => 'L'
       }
     end
 
     it 'must set currencies' do
       @repo.currencies.must_equal @currencies
+    end
+
+    it 'must return a roman numeral string given a hash of currency counts' do
+      @repo.to_roman('glob prok').must_equal 'IV'
     end
   end
